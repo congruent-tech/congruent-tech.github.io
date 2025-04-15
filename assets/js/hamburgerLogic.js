2e8a6a0b9743571ce8e89a9f76803369a3065626
congruent-tech <congruent-tech@users.noreply.github.com>
2025-04-15 18:42:04 +0000

    deploy: 189dd46887ea2dd72d86b6ab8d311d78d43d673c

---
+++ assets/js/hamburgerLogic.js
@@ -0,0 +1,11 @@
+<script>
+document.getElementById('menu-button').addEventListener('click', function() {
+    const menu = document.getElementById('mobile-menu');
+    const hamburgerIcon = document.getElementById('hamburger-icon');
+    const closeIcon = document.getElementById('close-icon');
+
+    menu.classList.toggle('hidden'); // Show/hide menu
+    hamburgerIcon.classList.toggle('hidden'); // Swap icons
+    closeIcon.classList.toggle('hidden'); // Swap icons
+});
+</script>
